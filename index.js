e993f91eb747c744d49dd7c2ec37c55b13ae7a81
Dramine7 <36771560+Dramine7@users.noreply.github.com>
2018-04-13 00:46:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -179,15 +179,19 @@ bot.on("message", message => {
     }
 
     //-----------------------------------------------------------
-      if(message.content.toLowerCase() == "/eastereggs"){
+   
+    if(message.content.toLowerCase() == "/eastereggs"){
         message.delete();
         var embed = new Discord.RichEmbed()
         .setThumbnail("https://media1.tenor.com/images/62fa490103c427b4c118336aa223b27a/tenor.gif?itemid=8292814")
         .setTitle("Les Easter Eggs")
+        .addBlankField()
+        .setDescription("**DISCLAIMER:** If you use any of the following Easter Eggs, that person gets pinged. Use with Caution!!!")
         .setColor(0xFF69B4)
         .addBlankField()
-        .addField("**Easter Egg Words General**","```/moustacheman - /colin - /codegod -/kay```")
+        .addField("**Easter Egg Words General**","```/moustacheman - /colin - /codegod - /kay```")
         .addField("**Easter Egg Words Darklight Collective**","```/elias - /zozo - /raishin - /amber - /lulu - /sith```")
+        .addBlankField()
         .setTimestamp()
         message.channel.sendMessage({embed});
     }
